bbc44be4bad246605d0379e84dd51ac3adfa2831
Rich <richard.repka@kodakalaris.com>
2026-01-20 14:58:34 -0500

    stream and buffers

--- Chapter06/01_simple_stream.js
+++ Chapter06/01_simple_stream.js
@@ -1,4 +1,6 @@
-// before we used read(), now we'll use streams
+// This program uses streams to read a file not read()
+// This program will read itsself and print out the
+// results
 
 var fs = require('fs');
 var contents;
@@ -6,26 +8,39 @@ var contents;
 // INCEPTION BWAAAAAAA!!!!
 var rs = fs.createReadStream("01_simple_stream.js");
 
+// Callback for a readable event
 rs.on('readable', () => {
     var str;
-    var d = rs.read();
+    var d = rs.read();          // There is somthing to ready, so read it
+
+    // Did we get anything?
     if (d) {
+        // Yes, what is it?
         if (typeof d == 'string') {
+            // This part is already of type string
             str = d;
         } else if (typeof d == 'object' && d instanceof Buffer) {
+            // Convert the passed JSON back to normal text
             str = d.toString('utf8');
         }
+
+        // Is there some string data?
         if (str) {
+            // Yes, is contents defined?
             if (!contents) 
+                // No, define it
                 contents = d;
             else
+                // Yes, add the segment to the overall data
                 contents += str;
         }
     }
 });
 
+// Callback for an end event
 rs.on('end', () => {
     console.log("read in the file contents: ");
+    // Convert the buffer into printable data
     console.log(contents.toString('utf8'));
 });
 
--- Chapter06/buffer_sidebar.js
+++ Chapter06/buffer_sidebar.js
@@ -1,25 +1,39 @@
+// Buffers can contain any kind of data
 
+//var b = new Buffer(10000);
+var b = Buffer.alloc(10000)
 
-var b = new Buffer(10000);
+// A 5 chinese character string which is really 15 bytes
 var str = "我叫王马克";
 
+// Put the string in the buffer
 b.write(str); // default is utf8, which is what we want
 
-console.log( b.length );
-
+// The buffer is 10,000 bytes long
+console.log("The string value:", b.toString('utf8'));
+console.log("Buffer size:     ", b.length );
 
 // byteLength is useful for working with UTF-8 and buffers
-console.log( str.length );
-console.log( Buffer.byteLength(str) );
+console.log("String length:   ", str.length );
+console.log("Byte length:     ", Buffer.byteLength(str) );
 
 
-var b1 = new Buffer("My name is ");
-var b2 = new Buffer("Marc");
+//var b1 = new Buffer("My name is ");
+// var b2 = new Buffer("Marc");
+// The length must be correct or alloc will truncate/iterate
+// the string 
+var b1 = Buffer.alloc(11,"My name is ");
+var b2 = Buffer.alloc(4, "Rich");
+
 var b3 = Buffer.concat([ b1, b2 ]);
-console.log(b3.toString('utf8'));
+console.log("b1:", b1.toString('utf8'), "b2:", b2.toString('utf8'), "b3:", b3.toString('utf8'));
 
+//var bb = new Buffer(100);
+var bb = Buffer.alloc(100);
 
-var bb = new Buffer(100);
+// Fill the buffer with zeros
 bb.fill("\0");
 
-console.log(bb.readInt8(0));
+console.log("Filled buffer", bb);
+//                               skipping 0 bytes from the start
+console.log("Reading buffer as an int ", bb.readInt8(0));
